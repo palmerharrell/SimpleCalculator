e8513419025caa8d4876632a9a9cc68658fd867a
Palmer Harrell <palmerharrell@me.com>
2016-04-21 10:18:06 -0500

    Set up exit condition

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -8,9 +8,27 @@ namespace SimpleCalculator
 {
   class Program
   {
+    // Should only receive user input and print output
     static void Main(string[] args)
     {
-      Console.WriteLine("testing...");
-    }
-  }
-}
+      Calculator calc = new Calculator();
+      bool endProgram = false;
+
+      while (!endProgram)
+      {
+        int calcCount = calc.numberofCalculations;
+
+        Console.Clear();
+        Console.Write("\n[{0}]> ", calcCount);
+        string userInput = Console.ReadLine();
+        if (userInput.ToUpper() == "QUIT" || userInput.ToUpper() == "EXIT")
+        {
+          endProgram = true;
+        }
+      }
+      
+
+      
+    } // Main
+  } // Program
+} // Namespace
