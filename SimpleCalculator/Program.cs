3018edd867571712625f4ea1a667a1831c073df3
Palmer Harrell <palmerharrell@me.com>
2016-04-21 10:25:50 -0500

    Set up conditions for last and lastq commands

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -18,12 +18,24 @@ namespace SimpleCalculator
       {
         int calcCount = calc.numberofCalculations;
 
-        Console.Clear();
+        //Console.Clear();
         Console.Write("\n[{0}]> ", calcCount);
         string userInput = Console.ReadLine();
+
         if (userInput.ToUpper() == "QUIT" || userInput.ToUpper() == "EXIT")
         {
           endProgram = true;
+          Console.WriteLine("\nGoodbye\n");
+        }
+        else if (userInput.ToUpper() == "LAST")
+        {
+          //Console.Clear();
+          Console.WriteLine("\nPrint result of last calculation here.\n");
+        }
+        else if (userInput.ToUpper() == "LASTQ")
+        {
+          //Console.Clear();
+          Console.WriteLine("\nPrint last calculation here.\n");
         }
       }
       
